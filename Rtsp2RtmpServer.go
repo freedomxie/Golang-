b7d0b1f1e877230e9f69e2124e96606202694027
freedomxie <xiejiajie@163.com>
2019-02-15 09:56:59 +0800

    Update Rtsp2RtmpServer.go
    防止子进程不能正常结束，采用进程组的方式
    创建一个新进程组队，防止误杀原进程

--- Rtsp2RtmpServer.go
+++ Rtsp2RtmpServer.go
@@ -28,7 +28,7 @@ var rtmpPort string
 /*
 ×  实时查看监控
 *  input:  camera uuid
-*  output: 0 正常 1 视频播放异常 2参数错误
+*  output: 0 正常 1 视频已播放 2参数错误
 */
 func playVideo(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
@@ -39,8 +39,8 @@ func playVideo(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "{\"status\":\"2\"}")
 		return
 	}
-	_rtmp := "rtmp://" + rtmpIp + "/" + _uuid
-	_playURL := "http://" + rtmpIp + ":" + rtmpPort + "/" + _uuid
+	_rtmp := "rtmp://" + rtmpIp + "/api/" + _uuid
+	_playURL := "http://" + rtmpIp + ":" + rtmpPort + "/api/" + _uuid
 
 	log.Println("uuid:", _uuid, " rtsp:", _rtsp, " rtmp:", _rtmp, " playURL:", _playURL)
 	_, ok := heartMap.Load(_uuid)
@@ -55,19 +55,23 @@ func playVideo(w http.ResponseWriter, r *http.Request) {
 		rtsp: _rtsp,
 		rtmp: _rtmp,
 		pid:  0}
+
 	pidMap.Store(_uuid, _player)
 	heartMap.Store(_uuid, time.Now().Unix())
 	go exeShell(_uuid, _rtsp, _rtmp)
+
 	result := "{\"status\":\"0\",\"playURL\":\"" + _playURL + "\"}"
+
 	io.WriteString(w, result)
 }
 
 /*
 ×  查看拉流状态+心跳
 *  input:  camera uuid
-*  output: 0 正常 1 视频播放异常 2参数错误
+*  output: 0 正常 1 视频进程已停止 2参数错误
 */
 func live(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
 	uuid := r.Form.Get("uuid")
 	if uuid == "" {
 		io.WriteString(w, "{\"status\":\"2\"}")
@@ -86,14 +90,14 @@ func killPid(uuid string) {
 	player, ok := pidMap.Load(uuid)
 	_player := player.(Player)
 	if ok {
-		err := syscall.Kill(-_player.pid, syscall.SIGKILL)
+		//err := syscall.Kill(_player.pid, syscall.SIGKILL)
+		err := syscall.Kill(-_player.pid, syscall.SIGKILL) //(防止子进程不能正常结束，采用进程组的方式)
 		if err != nil {
 			log.Println("kill subprocess fail", err)
 		} else {
 			log.Println("kill subprocess success, pid:", _player.pid)
 		}
-		syscall.Kill(_player.pid+1, syscall.SIGKILL)
-		syscall.Kill(_player.pid+2, syscall.SIGKILL)
+
 	} else {
 		log.Println("kill subprocess fail, not found pid:", _player.pid)
 	}
@@ -102,10 +106,10 @@ func killPid(uuid string) {
 func exeShell(uuid string, rtsp string, rtmp string) {
 	script := shellPath + " '" + rtsp + "' '" + rtmp + "'"
 	cmd := exec.Command("/bin/sh", "-c", script)
-        cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true} //（创建一个新进程组队，防止误杀原进程
+	fmt.Println("script:", script)
 	if err := cmd.Start(); err != nil {
-		log.Println(err.Error())
+		log.Println("start error.", err.Error())
 	}
 
 	pid := cmd.Process.Pid
@@ -118,10 +122,9 @@ func exeShell(uuid string, rtsp string, rtmp string) {
 		pidMap.Store(uuid, _player)
 	}
 	if err := cmd.Wait(); err != nil {
-		log.Println(err.Error())
+		log.Println("wait error.", err.Error())
 	}
 	clean(uuid)
-
 }
 
 func clean(uuid string) {
@@ -160,6 +163,7 @@ func main() {
 		log.Println("rtmp_ip:", "rtmp server ip")
 		log.Println("rtmp_port:", "rtmp server port")
 		os.Exit(0)
+
 	}
 	shellPath = os.Args[2]
 	rtmpIp = os.Args[3]
@@ -178,3 +182,8 @@ func main() {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
+
+//ffmpeg_run.sh
+//#!/bin/bash
+//# timeout is 10 second. 10000000 (microsecond)
+//ffmpeg -stimeout 15000000 -rtsp_transport tcp -re -i $1 -vcodec copy -acodec copy -f flv -y $2
